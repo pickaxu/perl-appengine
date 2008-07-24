11123d52c82b5d65fcbc9030b54f83f89bc6a5b7
brad <brad@danga.com>
2008-07-24 23:12:35 +0000

    cleanup the demo app.pl for presentation

--- app.pl
+++ app.pl
@@ -1,68 +1,25 @@
 #!/usr/bin/perl
-use strict;
 #
 # This should eventually feel like a CGI (or FastCGI?) environment.
 # same filedescriptors, etc.  Probably use another fd (3?) for the
 # apiproxy interface for now, a socketpair to parent process or
 # something.
 
+use strict;
+
 open(my $apiproxy, "<&=3") or die "Failed to open apiproxy fd: $!";
 
-#my $response = make_sync_call( $apiproxy, { foo => 'moose', 'hola' => 'orzorz' } );
-
-#use YAML::Syck;
-
-sub make_sync_call {
-    my ($sock, $data) = @_;
-    my $serialized = YAML::Syck::Dump($data);
-    my $length = length $serialized;
-    my $buf;
-    syswrite($sock, pack('N', $length), 4);
-    syswrite($sock, $serialized);
-
-    sysread($sock, $buf, 4);
-    $length = unpack('N', $buf);
-    sysread($sock, $buf, $length);
-    return YAML::Syck::Load($buf);
-}
-
-use CGI;
-
-my $cgi = CGI->new;
-
-
-print "<h1>Hello!</h1>You requested: $ENV{PATH_INFO}\n";
-
-
-print "Welcome to 1995, the era of the great technology of CGI!\n";
-warn "this is test from stderr";
-
-print "<p>Your cgi parameters:</p>";
-print "<ul>";
-for ($cgi->param) {
-    print "<li>$_: ".$cgi->param($_)."</li>";
-}
-
-print "</ul>";
-
+warn "mosorze from stderr";
+print "<h1>Hello!</h1>You requested path: $ENV{PATH_INFO}\n";
 
 syswrite($apiproxy, "Hello from app!\n");
-
-
 my $apiproxy_response = <$apiproxy>;
-
-print "<p>Apiproxy says: [$apiproxy_response]</p>\n";
-
-my $rv = eval qq{unlink "/etc/passwd"};
-
-print "The end.  unlink=$rv, error=$@\n";
-
-print "yatta!!\n";
-
+print "<p>Apiproxy response: [$apiproxy_response]</p>\n";
 
 syswrite($apiproxy, "Hello from app again, we are at $ENV{PATH_INFO}!\n");
-
 my $x = <$apiproxy>;
+print "<p>Apiproxy response: [$x]</p>";
 
-print "got ($x) back from apiproxy";
+my $rv = eval qq{unlink "/tmp/fooooo"};
+print "<p>The end.  unlink=$rv, error=$@</p>\n";
 
