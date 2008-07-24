ea93c58c64dcc1ef6c9e15657d1152f191669b39
chialiang <chialiang@98900f75-e84f-0410-b273-83ed227bf3fc>
2008-07-24 02:16:21 +0000

    * use http::server::simple::cgi * make app.pl be actually simple cgi script without parsing the requests.

--- trunk/app.pl
+++ trunk/app.pl
@@ -7,18 +7,13 @@
 
 open(my $apiproxy, "<&=3") or die "Failed to open apiproxy fd: $!";
 
-my $http_line = <STDIN>;
-die "Bogus HTTP request" unless $http_line =~ /^(GET) (\S+)(?: HTTP\/(\d+\.\d+))?/;
-my ($method, $path, $version) = ($1, $2, $3);
 
-while (<STDIN>) {
-    last if /^\s+$/;
-}
-
-print "HTTP/1.0 200 OK\n";
-print "Content-Type: text/html\n\n";
+print "moose blah!\n";
+warn "mosorze from stderr";
 
 syswrite($apiproxy, "Hello from app!\n");
+
+
 my $apiproxy_response = <$apiproxy>;
 
 print "<h1>Hello!</h1>You requested: $http_line\n";
@@ -28,3 +23,13 @@ print "<p>Apiproxy says: [$apiproxy_response]</p>\n";
 my $rv = eval qq{unlink "/etc/passwd"};
 
 print "The end.  unlink=$rv, error=$@\n";
+
+print "yatta!!\n";
+
+
+syswrite($apiproxy, "Hello from app again, we are at $ENV{PATHINFO}!\n");
+
+my $x = <$apiproxy>;
+
+print "got $x $apiproxy";
+
