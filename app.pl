3e53859ad09aedd007f0e1b1d9e3fa252c4491f3
chialiang <chialiang@98900f75-e84f-0410-b273-83ed227bf3fc>
2008-07-24 07:54:47 +0000

    welcome to 1995, CGI.pm works!

--- app.pl
+++ app.pl
@@ -8,16 +8,48 @@ use strict;
 
 open(my $apiproxy, "<&=3") or die "Failed to open apiproxy fd: $!";
 
+#my $response = make_sync_call( $apiproxy, { foo => 'moose', 'hola' => 'orzorz' } );
 
-print "moose blah!\n";
-warn "mosorze from stderr";
+#use YAML::Syck;
+
+sub make_sync_call {
+    my ($sock, $data) = @_;
+    my $serialized = YAML::Syck::Dump($data);
+    my $length = length $serialized;
+    my $buf;
+    syswrite($sock, pack('N', $length), 4);
+    syswrite($sock, $serialized);
+
+    sysread($sock, $buf, 4);
+    $length = unpack('N', $buf);
+    sysread($sock, $buf, $length);
+    return YAML::Syck::Load($buf);
+}
+
+use CGI;
+
+my $cgi = CGI->new;
+
+
+print "<h1>Hello!</h1>You requested: $ENV{PATH_INFO}\n";
+
+
+print "Welcome to 1995, the era of the great technology of CGI!\n";
+warn "this is test from stderr";
+
+print "<p>Your cgi parameters:</p>";
+print "<ul>";
+for ($cgi->param) {
+    print "<li>$_: ".$cgi->param($_)."</li>";
+}
+
+print "</ul>";
+
 
 syswrite($apiproxy, "Hello from app!\n");
 
 
 my $apiproxy_response = <$apiproxy>;
-
-print "<h1>Hello!</h1>You requested: $ENV{PATH_INFO}\n";
 
 print "<p>Apiproxy says: [$apiproxy_response]</p>\n";
 
@@ -32,5 +64,5 @@ syswrite($apiproxy, "Hello from app again, we are at $ENV{PATH_INFO}!\n");
 
 my $x = <$apiproxy>;
 
-print "got $x $apiproxy";
+print "got ($x) back from apiproxy";
 
