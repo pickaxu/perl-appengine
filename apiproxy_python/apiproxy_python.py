6ee05647190bee322a164460145ee0c59733a3ef
brad <brad@danga.com>
2008-07-31 21:37:50 +0000

    restructur, and map all of memcache

--- apiproxy_python/apiproxy_python.py
+++ apiproxy_python/apiproxy_python.py
@@ -16,18 +16,47 @@ from google.appengine.api.datastore import datastore_pb
 from google.appengine.api.datastore import entity_pb
 from google.appengine.api import api_base_pb
 
-MemcacheSetResponse = memcache_service_pb.MemcacheSetResponse
-MemcacheSetRequest = memcache_service_pb.MemcacheSetRequest
-MemcacheGetResponse = memcache_service_pb.MemcacheGetResponse
-MemcacheGetRequest = memcache_service_pb.MemcacheGetRequest
-MemcacheDeleteResponse = memcache_service_pb.MemcacheDeleteResponse
-MemcacheDeleteRequest = memcache_service_pb.MemcacheDeleteRequest
-MemcacheIncrementResponse = memcache_service_pb.MemcacheIncrementResponse
-MemcacheIncrementRequest = memcache_service_pb.MemcacheIncrementRequest
-MemcacheFlushResponse = memcache_service_pb.MemcacheFlushResponse
-MemcacheFlushRequest = memcache_service_pb.MemcacheFlushRequest
-MemcacheStatsRequest = memcache_service_pb.MemcacheStatsRequest
-MemcacheStatsResponse = memcache_service_pb.MemcacheStatsResponse
+# Some aliases:
+datastore = datastore_pb
+base = api_base_pb
+entity = entity_pb
+memcache = memcache_service_pb
+
+# This is lame that I have to manually maintain this table.  Is
+# there a better way?  At least we don't have to implement all
+# these ourselves. :)
+proto_class = {
+  "datastore_v3": {
+    "Get": (datastore.GetRequest, datastore.GetResponse),
+    "Put": (datastore.PutRequest, datastore.PutResponse),
+    "Delete": (datastore.DeleteRequest, base.VoidProto),
+    "RunQuery": (datastore.Query, datastore.QueryResult),
+    "Next": (datastore.NextRequest, datastore.QueryResult),
+    "Count": (datastore.Query, base.Integer64Proto),
+    "Explain": (datastore.Query, datastore.QueryExplanation),
+    "DeleteCursor": (datastore.Cursor, base.VoidProto),
+    "BeginTransaction": (base.VoidProto, datastore.Transaction),
+    "Commit": (datastore.Transaction, base.VoidProto),
+    "Rollback": (datastore.Transaction, base.VoidProto),
+    "GetSchema": (base.StringProto, base.VoidProto),
+    "CreateIndex": (entity.CompositeIndex, base.Integer64Proto),
+    "UpdateIndex": (entity.CompositeIndex, base.VoidProto),
+    "GetIndices": (base.StringProto, datastore.CompositeIndices),
+    "DeleteIndex": (entity.CompositeIndex, base.VoidProto),
+    },
+  "user": {},
+  "urlfetch": {},
+  "mail": {},
+  "memcache": {
+    "Get": (memcache.MemcacheGetRequest, memcache.MemcacheGetResponse),
+    "Set": (memcache.MemcacheSetRequest, memcache.MemcacheSetResponse),
+    "Delete": (memcache.MemcacheDeleteRequest, memcache.MemcacheDeleteResponse),
+    "Increment": (memcache.MemcacheIncrementRequest, memcache.MemcacheIncrementResponse),
+    "FlushAll": (memcache.MemcacheFlushRequest, memcache.MemcacheFlushResponse),
+    "Stats": (memcache.MemcacheStatsRequest, memcache.MemcacheStatsResponse),
+    },
+  "images": {},
+  }
 
 
 class MainPage(webapp.RequestHandler):
@@ -70,43 +99,6 @@ class DoRequest(webapp.RequestHandler):
     if not stub:
       self.response.out.write('<html><body>bogus service</body></html>')
       return
-
-    # Some aliases:
-    datastore = datastore_pb
-    base = api_base_pb
-    entity = entity_pb
-
-    # This is lame that I have to manually maintain this table.  Is
-    # there a better way?  At least we don't have to implement all
-    # these ourselves. :)
-    proto_class = {
-      "datastore_v3": {
-        "Get": (datastore.GetRequest, datastore.GetResponse),
-        "Put": (datastore.PutRequest, datastore.PutResponse),
-        "Delete": (datastore.DeleteRequest, base.VoidProto),
-        "RunQuery": (datastore.Query, datastore.QueryResult),
-        "Next": (datastore.NextRequest, datastore.QueryResult),
-        "Count": (datastore.Query, base.Integer64Proto),
-        "Explain": (datastore.Query, datastore.QueryExplanation),
-        "DeleteCursor": (datastore.Cursor, base.VoidProto),
-        "BeginTransaction": (base.VoidProto, datastore.Transaction),
-        "Commit": (datastore.Transaction, base.VoidProto),
-        "Rollback": (datastore.Transaction, base.VoidProto),
-        "GetSchema": (base.StringProto, base.VoidProto),
-        "CreateIndex": (entity.CompositeIndex, base.Integer64Proto),
-        "UpdateIndex": (entity.CompositeIndex, base.VoidProto),
-        "GetIndices": (base.StringProto, datastore.CompositeIndices),
-        "DeleteIndex": (entity.CompositeIndex, base.VoidProto),
-        },
-      "user": {},
-      "urlfetch": {},
-      "mail": {},
-      "memcache": {
-        "Get": (MemcacheGetRequest, MemcacheGetResponse),
-        "Set": (MemcacheSetRequest, MemcacheSetResponse),
-        },
-      "images": {},
-      }
 
     if not method in proto_class[service]:
       self.response.out.write('<html><body>unknown/unmapped method'
