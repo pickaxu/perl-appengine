8f9850dc4145fc0f47b86a944557358383c76812
brad <brad@danga.com>
2008-07-31 16:33:30 +0000

    parse the proto response

--- demos/prototest/app.pl
+++ demos/prototest/app.pl
@@ -11,28 +11,38 @@ use strict;
 use Data::Dumper;
 use AppEngine::APIProxy;
 use AppEngine::Service::Memcache;
+use AppEngine::Service::Images;
+use AppEngine::Service::URLFetch;
+# for datastore:
+use AppEngine::Service::Base;
+use AppEngine::Service::Entity;
+use AppEngine::Service::Datastore;
 
 print "<h1>Hello!</h1>";
 print "You requested path: $ENV{PATH_INFO}\n";
 
-my ($req, $item);
+my ($req, $res, $item);
 
 # add: (will work on the first try of SDK being up)
 $req = AppEngine::Service::MemcacheSetRequest->new;
+$res = AppEngine::Service::MemcacheSetResponse->new;
+
 $item = $req->add_item;
 $item->set_key("foo");
 $item->set_value("FOO_VALUE");
 $item->set_expiration_time(255);
 $item->set_set_policy(AppEngine::Service::MemcacheSetRequest::Item::SetPolicy::ADD);
-do_req("memcache", "Set", $req);
+if (do_req("memcache", "Set", $req, $res)) {
+    print "<pre>ENV = ", Dumper($res), "</pre>\n";
+}
 
 # set:
 $item->set_set_policy(AppEngine::Service::MemcacheSetRequest::Item::SetPolicy::SET);
-do_req("memcache", "Set", $req);
+do_req("memcache", "Set", $req, $res);
 
 # add again (won't work)
 $item->set_set_policy(AppEngine::Service::MemcacheSetRequest::Item::SetPolicy::ADD);
-do_req("memcache", "Set", $req);
+do_req("memcache", "Set", $req, $res);
 
 # just to show failing opcodes:
 my $rv = eval qq{unlink "/tmp/fooooo"};
@@ -41,15 +51,19 @@ print "<p>The end.  unlink=$rv, error=$@</p>\n";
 print "<pre>ENV = ", Dumper(\%ENV), "</pre>\n";
 
 sub do_req {
-    my ($service, $method, $proto) = @_;
-    my $res = eval {
+    my ($service, $method, $proto, $res) = @_;
+    my $res_bytes = eval {
         AppEngine::APIProxy::sync_call($service, $method, $proto);
     };
     if ($@) {
-        print "$service $method error was: <pre>", Dumper([$res, $@]), "</pre>";
-    } else {
-        my $escaped = $res;
-        $escaped =~ s/([^\w])/"\\x" . sprintf("%02x", ord($1))/eg;
-        print "<p>$service $method response was success: $escaped.</p>";
+        print "$service $method error was: <pre>", Dumper([$res_bytes, $@]), "</pre>";
+        return undef;
     }
+    my $escaped = $res_bytes;
+    $escaped =~ s/([^\w])/"\\x" . sprintf("%02x", ord($1))/eg;
+    print "<p>$service $method response was success: $escaped.</p>";
+    my $parsed = eval { $res->parse_from_string($res_bytes); 1 };
+    return 1 if $parsed;
+    print "<p>Failed to parse_from_string: $@\n</p>";
+    return 0;
 }
