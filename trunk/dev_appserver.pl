c702b5a760e156eef02ff05052b45446425e0169
davidsansome <davidsansome@98900f75-e84f-0410-b273-83ed227bf3fc>
2009-04-19 00:20:49 +0000

    --port commandline option

--- trunk/dev_appserver.pl
+++ trunk/dev_appserver.pl
@@ -18,6 +18,9 @@
 #    +--- client3 apiproxy client
 #
 
+use strict;
+use warnings;
+
 use FindBin qw($Bin);
 use lib "$Bin/lib";   # for running in dev without installing
 use AppEngine::Server;
@@ -34,6 +37,7 @@ Must contain a valid app.pl file.
 
 Options:
   --help, -h                 View this helpful message.
+  --port, -p                 Port for the server to run on. (Default 9000)
   TODO(bradfitz): steal more options from python dev_appserver
 
 END
@@ -41,16 +45,17 @@ END
     exit(1);
 }
 
-my $opt_help;
-my $bind_address;
-usage() unless GetOptions(
-    "address|a=s" => \$bind_address,
-    "help|h"      => \$opt_help,
+my %opts;
+usage() unless GetOptions(\%opts,
+    "help|h"       => sub { usage() },
+    "address|a=s",
+    "port|p=i",
     # TODO(bradfitz): clone more of the interesting
     # options from python_sdk_partial/dev_appserver.py
     );
 
-usage() if $opt_help;
+# Default options
+$opts{port} ||= 9000;
 
 my $app_dir = shift;
 usage() unless $app_dir;
@@ -58,4 +63,4 @@ die "Directory doesn't exist.\n" unless -d $app_dir;
 die "Forbidden characters in directory name.\n" if $app_dir =~ /[^\w\-\/\.]/;
 die "Directory doesn't contain an app.pl file.\n" unless -e "$app_dir/app.pl";
 
-AppEngine::Server->new(9000, $app_dir)->run;
+AppEngine::Server->new($opts{port}, $app_dir)->run;
