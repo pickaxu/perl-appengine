aaf4a9da60409c41df01c44fcdfa50b4eb8bb4f2
davidsansome <davidsansome@98900f75-e84f-0410-b273-83ed227bf3fc>
2009-04-19 22:07:36 +0000

    Move the apiproxy magic around so it can be used from tests without starting up Server.pm

--- trunk/lib/AppEngine/APIProxy.pm
+++ trunk/lib/AppEngine/APIProxy.pm
@@ -3,9 +3,20 @@
 # to the python sdk to use the python mock implementations.
 
 package AppEngine::APIProxy;
+
+use warnings;
 use strict;
 
-open(my $apiproxy, "<&=3") or die "Failed to open apiproxy fd: $!";
+use Data::Dumper;
+use HTTP::Request::Common;
+use LWP::UserAgent;
+
+use base qw(Exporter);
+our @EXPORT_OK = qw(sync_call become_apiproxy_client);
+
+our $bypass_client = 0;
+
+my $apiproxy;
 
 sub sync_call {
     if (scalar @_ == 1) {
@@ -21,20 +32,85 @@ sub sync_call {
         $message = $message->serialize_to_string;
     }
     die "Bogus message" if ref($message);
-    my $len = length $message;
-    my $request = "apiproxy $service $method $len\n$message";
-    syswrite($apiproxy, $request);
-    my $res = <$apiproxy>;
-    die "Bogus apiproxy response: $res" unless
-        $res =~ s/^apiresult ([01]) (\d+)\n//;
-    my ($success, $length) = ($1, $2);
-    my $body;
-    read($apiproxy, $body, $length) == $length or die;
-    if ($success) {
-        return $body;
-    } else {
-        die "APIProxy error: $body\n";
+
+    my ($success, $body);
+    if ($bypass_client) {
+        ($success, $body) = _make_request($service, $method, $message);
     }
+    else {
+        unless ($apiproxy) {
+            open($apiproxy, "<&=3") or die "Failed to open apiproxy fd: $!";
+        }
+
+        my $len = length $message;
+        my $request = "apiproxy $service $method $len\n$message";
+        syswrite($apiproxy, $request);
+        my $res = <$apiproxy>;
+        die "Bogus apiproxy response: $res" unless
+            $res =~ s/^apiresult ([01]) (\d+)\n//;
+        ($success, $len) = ($1, $2);
+        read($apiproxy, $body, $len) == $len or die;
+    }
+
+    return $body if $success;
+    die "APIProxy error: $body\n";
+}
+
+sub become_apiproxy_client {
+    my $socket = shift;
+
+    select($socket);
+    $| = 1;
+    while (my $cmd = <$socket>) {
+        chomp $cmd;
+        unless ($cmd =~ /^apiproxy (\S+) (\S+) (\d+)/) {
+            print STDERR "Unknown apiproxy line: [$cmd]\n";
+            print $socket "You said: $cmd\n";
+            next;
+        }
+        my ($service, $method, $request_length) = ($1, $2, $3);
+        my $pb_request;
+        my $rv = read($socket, $pb_request, $request_length);
+        if ($rv != $request_length) {
+            die "Failed to read entire request from app.";
+        }
+
+        my ($success, $body) = _make_request($service, $method, $pb_request);
+
+        print $socket "apiresult $success ", length($body), "\n$body";
+    }
+}
+
+sub _make_request {
+    my ($service, $method, $pb_request) = @_;
+
+    my $ua = LWP::UserAgent->new;
+
+    my $req = POST "http://127.0.0.1:8080/do_req", [
+        service => $service,
+        method => $method,
+        request => $pb_request, ];
+
+    print STDERR "Sending request: ", $req->as_string, "\n";
+    my $res = $ua->request($req);
+
+    my $success = $res->is_success ? 1 : 0;
+    my $body;
+    if ($success) {
+        if ($res->content =~ /^Response: \[(.*)\]\s*$/s) {
+            $body = $1;
+        } else {
+            $body = $res->content;
+            $success = 0;
+        }
+    } else {
+        $body = $res->status_line;
+    }
+
+    print STDERR "Got apiproxy result (for $service, $method) of success=$success:\n";
+    print STDERR Dumper($body);
+
+    return ($success, $body);
 }
 
 # old simple echo test.
--- trunk/lib/AppEngine/Server.pm
+++ trunk/lib/AppEngine/Server.pm
@@ -24,6 +24,7 @@ use base qw(HTTP::Server::Simple HTTP::Server::Simple::CGI::Environment);
 use strict;
 use warnings;
 
+use AppEngine::APIProxy qw(become_apiproxy_client);
 use AppEngine::AppConfig;
 use IO::Socket::INET;
 use English;
@@ -32,8 +33,6 @@ use File::Spec::Functions qw(catfile);
 use POSIX qw(dup2);
 use Socket;
 use IPC::Run 'start';
-use LWP::UserAgent;
-use HTTP::Request::Common;
 use Data::Dumper;
 
 our $VERSION = "0.01";
@@ -147,48 +146,5 @@ sub _handle_script {
          "-I$appdir", "$appdir/$script";
 }
 
-sub become_apiproxy_client {
-    my $socket = shift;
 
-    my $ua = LWP::UserAgent->new;
-
-    select($socket);
-    $| = 1;
-    while (my $cmd = <$socket>) {
-        chomp $cmd;
-        unless ($cmd =~ /^apiproxy (\S+) (\S+) (\d+)/) {
-            print STDERR "Unknown apiproxy line: [$cmd]\n";
-            print $socket "You said: $cmd\n";
-            next;
-        }
-        my ($service, $method, $request_length) = ($1, $2, $3);
-        my $pb_request;
-        my $rv = read($socket, $pb_request, $request_length);
-        if ($rv != $request_length) {
-            die "Failed to read entire request from app.";
-        }
-        my $req = POST "http://127.0.0.1:8080/do_req", [
-            service => $service,
-            method => $method,
-            request => $pb_request, ];
-        print STDERR "Sending request: ", $req->as_string, "\n";
-        my $res = $ua->request($req);
-
-        my $success = $res->is_success ? 1 : 0;
-        my $body;
-        if ($success) {
-            if ($res->content =~ /^Response: \[(.*)\]\s*$/s) {
-                $body = $1;
-            } else {
-                $body = $res->content;
-                $success = 0;
-            }
-        } else {
-            $body = $res->status_line;
-        }
-        print STDERR "Got apiproxy result (for $service, $method) of success=$success:\n";
-        print STDERR Dumper($body);
-        print $socket "apiresult $success ", length($body), "\n$body";
-    }
-}
 
