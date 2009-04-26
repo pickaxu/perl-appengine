2e87ec75806b7653ff5e4b08cc800b5adb223876
davidsansome <davidsansome@98900f75-e84f-0410-b273-83ed227bf3fc>
2009-04-26 00:49:24 +0000

    POD for Key.pm.  Mostly stolen from the python docs.

--- trunk/lib/AppEngine/API/Datastore/Key.pm
+++ trunk/lib/AppEngine/API/Datastore/Key.pm
@@ -3,9 +3,64 @@ package AppEngine::API::Datastore::Key;
 use strict;
 use warnings;
 
+=head1 NAME
+
+AppEngine::API::Datastore::Key - Perl version of google.appengine.ext.db.Key
+
+=head1 SYNOPSIS
+
+    use AppEngine::API::Datastore::Key qw(key_from_path);
+    
+    my $key = AppEngine::API::Datastore::Key->new($encoded_str);
+    my $key = key_from_path(['User', 'Boris', 'Address', 9876]);
+    
+    print $key->app;
+    print $key->kind;
+    print $key->id_or_name;
+    
+    print $key->str;
+
+=head1 DESCRIPTION
+
+An instance of the Key class represents a unique key for a datastore entity.
+
+=head2 Exports
+
+The following functions can be exported by request:
+
+    key_from_path
+
+=head1 METHODS
+
+=over
+
+=cut
+
 use AppEngine::Service::Entity;
 use Carp;
 use MIME::Base64::URLSafe;
+
+use base qw(Exporter);
+our @EXPORT_OK = qw(key_from_path);
+
+=item new ( encoded_string )
+
+Creates a Key object from its encoded string representation.
+
+A key can be encoded to a string by calling its key() method.
+A string-encoded key is an opaque value using characters safe for including in
+URLs.
+The string-encoded key can be converted back to a Key object by passing it to
+the Key constructor (the C<encoded_string> argument).
+
+B<Note:> A string-encoded key can be converted back to the raw key data.
+This makes it easy to guess other keys when one is known.
+While string-encoded key values are safe to include in URLs, an application
+should only do so if key guessability is not an issue.
+
+C<encoded> is the str form of a Key instance to convert back into a Key.
+
+=cut
 
 sub new {
     my ($pkg, $encoded) = @_;
@@ -15,6 +70,31 @@ sub new {
 
     return _from_pb($pb);
 }
+
+=item from_path ( path [, parent => parent_key ] )
+
+Builds a new Key object from an ancestor path of one or more entity keys.
+
+A path represents the hierarchy of parent-child relationships for an entity. 
+Each entity in the path is represented the entity's kind, and either its numeric
+ID or its key name.
+The full path represents the entity that appears last in the path, with its
+ancestors (parents) as preceding entities.
+
+For example, the following call creates a key for an entity of kind Address with
+the numeric ID 9876 whose parent is an entity of kind User with the named key
+'Boris':
+
+    my $key = AppEngine::API::Datastore::Key::from_path(['User', 'Boris', 'Address', 9876])
+
+C<path> is an arrayref containing the path from the root entity to the subject.
+Each entity in the path is represented by two elements in the list: the name of
+the kind, and the key name or ID of the entity of that kind.
+
+C<parent_key> can be used to specify a parent entity to prepend to the given
+path.
+
+=cut
 
 sub from_path {
     my $ref = AppEngine::Service::Entity::Reference->new;
@@ -67,29 +147,43 @@ sub from_path {
     return $self;
 }
 
-sub _from_pb {
-    my ($pb) = @_;
+=item key_from_path ( path [, parent => parent_key ] )
 
-    my $self = {
-        _ref => $pb,
-    };
-    bless $self, __PACKAGE__;
-    return $self;
-}
+Alias for from_path() that can be exported into the caller's namespace.
 
+    use AppEngine::API::Datastore::Key qw(key_from_path);
+    my $key = key_from_path(['User', 'Boris', 'Address', 9876])
+
+=cut
+
+*key_from_path = \&from_path;
+
+=item app
+
+Returns the name of the application that stored the data entity.
+
+=cut
 
 sub app {
     return $_[0]->{_ref}->app;
 }
 
-sub _last_element {
-    my $i = $_[0]->{_ref}->path->element_size - 1;
-    return $_[0]->{_ref}->path->elements->[$i];
-}
+=item kind
+
+Returns the kind of the data entity, as a string.
+
+=cut
 
 sub kind {
     return $_[0]->_last_element->type;
 }
+
+=item name
+
+Returns the name of the data entity, or undef if the entity does not have a
+name.
+
+=cut
 
 sub name {
     my $e = $_[0]->_last_element;
@@ -97,19 +191,48 @@ sub name {
     return;
 }
 
+=item id
+
+Returns the numeric ID of the data entity, as an integer, or undef if the
+entity does not have a numeric ID.
+
+=cut
+
 sub id {
     my $e = $_[0]->_last_element;
     return $e->id if $e->has_id;
     return;
 }
 
+=item id_or_name
+
+Returns the name or numeric ID of the data entity, whichever it has, or undef
+if the entity has neither a name nor a numeric ID.
+
+=cut
+
 sub id_or_name {
     return $_[0]->id || $_[0]->name;
 }
 
+=item has_id_or_name
+
+Returns true if the entity has either a name or a numeric ID.
+
+=cut
+
 sub has_id_or_name {
     return $_[0]->_last_element->has_id || $_[0]->_last_element->has_name;
 }
+
+=item path
+
+Returns the complete path for this entity as an array.
+Suitable for passing to key_from_path().
+
+    my $key2 = key_from_path( [ $key1->path ] );
+
+=cut
 
 sub path {
     my $self = shift;
@@ -128,6 +251,13 @@ sub path {
     return @ret;
 }
 
+=item parent
+
+Returns the Key of the data entity's parent entity, or None if the entity has
+no parent.
+
+=cut
+
 sub parent {
     my $self = shift;
     my @path = $self->path;
@@ -139,6 +269,24 @@ sub parent {
     return AppEngine::API::Datastore::Key::from_path(\@path);
 }
 
+=item str
+
+Returns an encoded string representation of this Key.
+Suitable for passing to new().
+
+    my $key2 = AppEngine::API::Datastore::Key->new( $key1->str );
+
+=cut
+
+sub str {
+    my $self = shift;
+
+    return urlsafe_b64encode($self->{_ref}->serialize_to_string);
+}
+
+
+# Internal methods
+
 sub entity_group {
     my $self = shift;
     my @path = $self->path;
@@ -146,10 +294,19 @@ sub entity_group {
     return AppEngine::API::Datastore::Key::from_path([@path[0, 1]]);
 }
 
-sub str {
-    my $self = shift;
+sub _last_element {
+    my $i = $_[0]->{_ref}->path->element_size - 1;
+    return $_[0]->{_ref}->path->elements->[$i];
+}
 
-    return urlsafe_b64encode($self->{_ref}->serialize_to_string);
+sub _from_pb {
+    my ($pb) = @_;
+
+    my $self = {
+        _ref => $pb,
+    };
+    bless $self, __PACKAGE__;
+    return $self;
 }
 
 sub _to_pb {
