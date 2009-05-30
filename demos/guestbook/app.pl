91d598a62ec58d933329f3ea8d1b5681eaf0fd80
davidsansome <davidsansome@98900f75-e84f-0410-b273-83ed227bf3fc>
2009-05-30 20:56:20 +0000

    Upgrade the guestbook demo to use the users service

--- demos/guestbook/app.pl
+++ demos/guestbook/app.pl
@@ -11,6 +11,7 @@ use warnings;
 use AppEngine::API::Datastore;
 use AppEngine::API::Datastore::Entity;
 use AppEngine::API::Datastore::Query;
+use AppEngine::API::Users;
 use AppEngine::APIProxy;
 use CGI;
 use Data::Dumper;
@@ -21,6 +22,7 @@ print $cgi->header;
 if ($ENV{REQUEST_METHOD} eq 'POST') {
     my $greeting = AppEngine::API::Datastore::Entity->new('Greeting');
     $greeting->{content} = $cgi->param('content');
+    $greeting->{author} = users_get_current_user();
 
     AppEngine::API::Datastore::put($greeting);
 }
@@ -29,7 +31,11 @@ print '<html><body>';
 
 my $query = AppEngine::API::Datastore::Query->new('Greeting');
 while (my $greeting = $query->fetch) {
-    print 'An anonymous person wrote:';
+    if ($greeting->{author}) {
+        print '<b>' . $greeting->{author}->nickname . '</b> wrote:';
+    } else {
+        print 'An anonymous person wrote:';
+    }
     # TODO(davidsansome): encode html entities
     print '<blockquote>', $greeting->{content}, '</blockquote>';
 }
